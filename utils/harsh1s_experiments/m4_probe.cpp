a983d26e65df35efced87700ab8f1c605148bd3a
Harsh1s <thisishraj@gmail.com>
2025-06-22 23:37:10 +0530

    Add clickhouse yearlong experiment case 05

--- utils/harsh1s_experiments/m4_probe.cpp
+++ utils/harsh1s_experiments/m4_probe.cpp
@@ -13,6 +13,7 @@ constexpr Case kCases[] = {
     {"2025-06-08", 569745u},
     {"2025-06-15", 646906u},
     {"2025-06-21", 371629u},
+    {"2025-06-22", 566098u},
 };
 
 constexpr std::size_t case_count() { return sizeof(kCases) / sizeof(kCases[0]); }
